e1e00e006d0fea257570b93ea60540c12b725210
Petr Fiala <petr.fiala@atlas.cz>
2023-12-24 02:23:00 +0100

    no arg constructor fot work

--- src/main/java/com/towersly/app/library/model/Work.java
+++ src/main/java/com/towersly/app/library/model/Work.java
@@ -2,10 +2,12 @@ package com.towersly.app.library.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class Work {
